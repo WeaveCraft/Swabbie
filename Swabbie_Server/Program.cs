ab5968a681b4b47226063f534c6b2e2814ffedfb
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-08-04 10:34:03 +0200

    Added Dropdown Menu for selecting language for website. Swedish was also added. Removed the "old" way of changing language so when changing language you stay on your current page instead of going back to Home.

--- Swabbie_Server/Program.cs
+++ Swabbie_Server/Program.cs
@@ -11,13 +11,28 @@ builder.Services.AddMudBlazorDialog();
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddLocalization(opt => opt.ResourcesPath = "Culture");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IIncomeRepository, IncomeRepository>();
 builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+RequestLocalizationOptions GetLocalizationOptions()
+{
+    var cultures = builder.Configuration.GetSection("Cultures")
+        .GetChildren().ToDictionary(x => x.Key, x => x.Value);
+
+    var supportedCultures = cultures.Keys.ToArray();
+
+    var localizationOptions = new RequestLocalizationOptions()
+        .AddSupportedCultures(supportedCultures)
+        .AddSupportedUICultures(supportedCultures);
+
+    return localizationOptions;
+}
 
 var app = builder.Build();
 
@@ -29,14 +44,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-var localizeOptions = new RequestLocalizationOptions()
-    .SetDefaultCulture("en-US")
-    .AddSupportedCultures("en-US", "es-ES", "sv-SE")
-    .AddSupportedUICultures("en-US", "es-ES", "sv-SE");
-app.UseRequestLocalization(localizeOptions);
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseRequestLocalization(GetLocalizationOptions());
 
 app.UseRouting();
 
